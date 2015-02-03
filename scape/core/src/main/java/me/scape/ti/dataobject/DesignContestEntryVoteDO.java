7cf4a5f53fe22b61df936d834f10128a5c6e6c8d
liufei_it <liufei_it@126.com>
2015-02-03 10:22:03 +0800

    景观比赛

--- scape/core/src/main/java/me/scape/ti/dataobject/DesignContestEntryVoteDO.java
+++ scape/core/src/main/java/me/scape/ti/dataobject/DesignContestEntryVoteDO.java
@@ -32,7 +32,7 @@ public class DesignContestEntryVoteDO implements Serializable {
 	private Long id;
 
 	@Column(name = "entry_id", nullable = false)
-	private Integer entry_id;// 作品ID
+	private Long entry_id;// 作品ID
 
 	@Column(name = "voter_id", nullable = false)
 	private Long voter_id;// 投票者
@@ -45,4 +45,92 @@ public class DesignContestEntryVoteDO implements Serializable {
 	@Column(name = "gmt_modified", nullable = true)
 	private Date gmt_modified;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getEntry_id() {
+		return entry_id;
+	}
+
+	public void setEntry_id(Long entry_id) {
+		this.entry_id = entry_id;
+	}
+
+	public Long getVoter_id() {
+		return voter_id;
+	}
+
+	public void setVoter_id(Long voter_id) {
+		this.voter_id = voter_id;
+	}
+
+	public Date getGmt_created() {
+		return gmt_created;
+	}
+
+	public void setGmt_created(Date gmt_created) {
+		this.gmt_created = gmt_created;
+	}
+
+	public Date getGmt_modified() {
+		return gmt_modified;
+	}
+
+	public void setGmt_modified(Date gmt_modified) {
+		this.gmt_modified = gmt_modified;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((entry_id == null) ? 0 : entry_id.hashCode());
+		result = prime * result + ((gmt_created == null) ? 0 : gmt_created.hashCode());
+		result = prime * result + ((gmt_modified == null) ? 0 : gmt_modified.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((voter_id == null) ? 0 : voter_id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DesignContestEntryVoteDO other = (DesignContestEntryVoteDO) obj;
+		if (entry_id == null) {
+			if (other.entry_id != null)
+				return false;
+		} else if (!entry_id.equals(other.entry_id))
+			return false;
+		if (gmt_created == null) {
+			if (other.gmt_created != null)
+				return false;
+		} else if (!gmt_created.equals(other.gmt_created))
+			return false;
+		if (gmt_modified == null) {
+			if (other.gmt_modified != null)
+				return false;
+		} else if (!gmt_modified.equals(other.gmt_modified))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (voter_id == null) {
+			if (other.voter_id != null)
+				return false;
+		} else if (!voter_id.equals(other.voter_id))
+			return false;
+		return true;
+	}
 }
