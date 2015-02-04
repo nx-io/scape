d5d71788e6388ef5b1df9e84eda88e523c9c9598
liufei_it <liufei_it@126.com>
2015-02-04 20:59:04 +0800

    srv

--- scape/build_wjz.sh
+++ scape/build_wjz.sh
@@ -2,6 +2,6 @@
 
 mvn clean && mvn install  -Dmaven.test.skip=true
 echo "build success"
-#scp -r api/target/scape-api.war root@aliyun:~/jetty/app/
-scp -r api/target/scape-api.war root@wjz:~/jetty/app/
+#scp -r api/target/scape-api.war root@aliyun:~/jetty/apps/
+scp -r api/target/scape-api.war root@wjz:~/jetty/apps/
 echo "deploy success"
